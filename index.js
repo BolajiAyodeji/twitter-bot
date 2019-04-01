1f5b1ffa83741c6e6662abfabd487d7a9f6e2d9d
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-04-01 01:17:46 +0100

    setup dotenv

---
+++ index.js
@@ -0,0 +1,12 @@
+
+require('dotenv').config()
+
+let Twit = require('twit');
+
+let bot = new Twit({
+  consumer_key: process.env.BOLAJI_CONSUMER_KEY,
+  consumer_secret: process.env.BOLAJI_CONSUMER_SECRET,
+  access_token: process.env.BOLAJI_ACCESS_TOKEN,
+  access_token_secret: process.env.BOLAJI_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60*1000
+})
